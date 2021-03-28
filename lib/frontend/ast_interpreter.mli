78b065e4fa82b7438b1e32c4eba7599acd208d3a
Ryan <ryan.guo99@gmail.com>
2021-03-28 00:31:49 -0400

    Update docs for Ast_interpreter module

--- lib/frontend/ast_interpreter.mli
+++ lib/frontend/ast_interpreter.mli
@@ -1,4 +1,5 @@
 
-(** [interp_struct ctx struct] interprets [struct] in [ctx]; 
-    returns the updated context. *)
+(** [interp_struct ctx struct] interprets [struct] in [ctx].
+    Supported native functions:
+    - [println e] prints out result of each [e] on newline and returns it. *)
 val interp_struct : Ast.structure -> unit
