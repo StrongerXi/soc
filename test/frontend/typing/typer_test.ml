71d369ed847045d4f64e5bf465ae45f78a760259
Ryan <ryan.guo99@gmail.com>
2021-04-04 09:37:52 -0400

    Forgot to update the name infer to typer, apparently should've greped test folder too

--- test/frontend/typing/typer_test.ml
+++ test/frontend/typing/typer_test.ml
@@ -5,7 +5,7 @@ let _get_full_path (filename : string) : string =
   String.append "../../../test/frontend/typing/resources/" filename
 ;;
 
-let _check_infer_struct (filepath_no_suffix : string) : unit =
+let _check_typer_struct (filepath_no_suffix : string) : unit =
   let filepath = String.append filepath_no_suffix ".soml" in
   let result =
     match Driver.type_file filepath with
@@ -18,12 +18,12 @@ let _check_infer_struct (filepath_no_suffix : string) : unit =
 ;;
 
 
-let tests = OUnit2.(>:::) "infer_test" [
+let tests = OUnit2.(>:::) "typer_test" [
 
     OUnit2.(>::) "test_integration" (fun _ ->
-        _check_infer_struct (_get_full_path "infer_input_mixed");
-        _check_infer_struct (_get_full_path "annot");
-        _check_infer_struct (_get_full_path "annot_error");
+        _check_typer_struct (_get_full_path "typer_input_mixed");
+        _check_typer_struct (_get_full_path "annot");
+        _check_typer_struct (_get_full_path "annot_error");
       );
   ]
 
