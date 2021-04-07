ea47187cb24e9a0f9ede758694961db8e6c51add
Ryan <ryan.guo99@gmail.com>
2021-04-07 17:41:38 -0400

    Add integration test for CIR

---
+++ test/backend/cir_test.ml
@@ -0,0 +1,29 @@
+open Pervasives
+
+(* [filename] can assume CWD is where this file is *)
+let _get_full_path (filename : string) : string =
+  String.append "../../../test/backend/cir-resources/" filename
+;;
+
+let _check_cir_pp (filepath_no_suffix : string) : unit =
+  let filepath = String.append filepath_no_suffix ".soml" in
+  let result =
+    match Driver.cir_file filepath with
+    | Error msg -> msg
+    | Ok cir -> Pretty.pp_cir cir
+  in
+  let expect_path = String.append filepath_no_suffix ".expect" in
+  let actual_path = String.append filepath_no_suffix ".actual" in
+  Test_aux.check_and_output_str result expect_path actual_path;
+;;
+
+
+let tests = OUnit2.(>:::) "cir_test" [
+
+    OUnit2.(>::) "test_integration" (fun _ ->
+        _check_cir_pp (_get_full_path "freevar")
+      );
+  ]
+
+let _ =
+  OUnit2.run_test_tt_main tests
