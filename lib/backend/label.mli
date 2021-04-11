4608ac6cf305192c583c4ffda0acbf410b914bab
Ryan <ryan.guo99@gmail.com>
2021-04-10 23:36:51 -0400

    Add Label module in backend to reprensent and generate unique labels

---
+++ lib/backend/label.mli
@@ -0,0 +1,31 @@
+open Pervasives
+
+(** A [t] represents a label in low level representations *)
+type t
+
+(** A [manager] can generate unique [t] and associate string with [t] *)
+type manager
+
+
+(** An [init_manager] is a bare minimum [manager] *)
+val init_manager : manager
+
+(** [get_or_gen manager s] retrieves the [t] bound to [s].
+    Return [None] if [s] is not bound in [manager]. *)
+val get : manager -> string -> t option
+
+(** [gen manager prefix] generates a unique [t] that starts with [prefix]
+    and won't bind it with any string in output [manager].  
+    ASSUME [prefix] doesn't contain space. *)
+val gen : manager -> string -> (manager * t)
+
+(** [gen_and_bind manager name] is like [gen manager], but output manager
+    binds [name] with the returned [t] *)
+val gen_and_bind : manager -> string -> (manager * t)
+
+(** [create_native name] returns a label for externally defined function.
+    It's guaranteed to be unique if [name] is unique *)
+val create_native : string -> t
+
+(** [to_string t] returns a string representation of [t] *)
+val to_string : t -> string
