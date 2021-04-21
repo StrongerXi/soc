a8c682b2b4355076408c40ebaf10cb5ce04cf03d
Ryan <ryan.guo99@gmail.com>
2021-04-21 17:28:50 -0400

    Update application code for soc to use entire compiler pipeline

--- bin/soc.ml
+++ bin/soc.ml
@@ -2,6 +2,13 @@ open Pervasives
 
 let () =
   let filename = Sys.argv.(1) in
-  match Driver.type_file filename with
-  | Ok ast -> Io.println (Pretty.pp_ast_structure ast)
+  match Driver.lir_file filename with
+  | Ok lir_prog ->
+    let x86_prog = Driver.lir_to_x86 lir_prog in
+    let x86_prog_str = X86.prog_to_str x86_prog in
+    let extless_filename, _ = Filename.split_extension filename in
+    let output_filename = String.append extless_filename ".s" in
+    Io.write_file output_filename x86_prog_str
+
   | Error msg -> Io.println msg
+;;
