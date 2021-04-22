b974c8be41b0c6260442fe3929746e381aecbcae
Ryan <ryan.guo99@gmail.com>
2021-04-22 16:34:20 -0400

    Add utility function in custom Int module for calculating offset for alignment

--- lib/stdlib/int.ml
+++ lib/stdlib/int.ml
@@ -11,6 +11,12 @@ let ceil_div n1 n2 =
   (n1 + n2 - 1) / n2
 ;;
 
+let offset_to_align n alignment =
+  let extra = n mod alignment in
+  if extra = 0 then 0
+  else alignment - extra
+;;
+
 let compare n1 n2 =
   if n1 = n2 then 0
   else if n1 > n2 then 1
--- test/soc/stdlib/int_test.ml
+++ test/soc/stdlib/int_test.ml
@@ -21,6 +21,14 @@ let tests = OUnit2.(>:::) "int_test" [
         OUnit2.assert_equal 0 (Int.ceil_div 0 5);
       );
 
+    OUnit2.(>::) "test_offset_to_align" (fun _ ->
+        OUnit2.assert_equal 0 (Int.offset_to_align 0 5);
+        OUnit2.assert_equal 4 (Int.offset_to_align 1 5);
+        OUnit2.assert_equal 0 (Int.offset_to_align 5 5);
+        OUnit2.assert_equal 1 (Int.offset_to_align 9 5);
+        OUnit2.assert_equal 0 (Int.offset_to_align 7 1);
+      );
+
     OUnit2.(>::) "test_compare" (fun _ ->
         OUnit2.assert_equal true ((Int.compare 5 5) = 0);
         OUnit2.assert_equal true ((Int.compare 3 7) < 0);
